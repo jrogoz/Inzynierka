a82c5124c5d11ab68b71cf9f586ebc9e2ef6d269
jrogoz <joanna.rogoz33@gmail.com>
2020-11-23 22:46:24 +0100

    Add syn and rec

---
+++ adventure/__main__.py
@@ -0,0 +1,61 @@
+"""Offer Adventure at a custom command prompt.
+
+Copyright 2010-2015 Brandon Rhodes.  Licensed as free software under the
+Apache License, Version 2.0 as detailed in the accompanying README.txt.
+
+"""
+import argparse
+import os
+import re
+import readline
+import sys
+from time import sleep
+from adventure import load_advent_dat
+from adventure.game import Game
+from adventure.speech import synthesis, recognition
+
+BAUD = 1200
+
+def baudout(s):
+    out = sys.stdout
+    for c in s:
+        sleep(9. / BAUD)  # 8 bits + 1 stop bit @ the given baud rate
+        out.write(c)
+        out.flush()
+    synthesis(s.lower())
+
+def loop(args):
+    parser = argparse.ArgumentParser(
+        description='Adventure into the Colossal Caves.',
+        prog='{} -m adventure'.format(os.path.basename(sys.executable)))
+    parser.add_argument(
+        'savefile', nargs='?', help='The filename of game you have saved.')
+    args = parser.parse_args(args)
+
+    if args.savefile is None:
+        game = Game()
+        load_advent_dat(game)
+        game.start()
+        baudout(game.output)
+
+
+    else:
+        game = Game.resume(args.savefile)
+        baudout('GAME RESTORED\n')
+
+    while not game.is_finished:
+        # line = input('> ').lower()
+        # words = re.findall(r'\w+', line)
+        line = recognition()
+        line = line.lower()
+        words = re.findall(r'\w+', line)
+        print(words)
+        if words:
+            baudout(game.do_command(words))
+
+
+if __name__ == '__main__':
+    try:
+        loop(sys.argv[1:])
+    except EOFError:
+        pass
---
+++ adventure/speech.py
@@ -0,0 +1,48 @@
+"""Parse the original PDP ``advent.dat`` file.
+
+Copyright 2010-2015 Brandon Rhodes.  Licensed as free software under the
+Apache License, Version 2.0 as detailed in the accompanying README.txt.
+
+"""
+
+import speech_recognition as sr
+import pyttsx3
+import pyaudio
+# import engineio
+
+def synthesis(text):
+
+    engineio = pyttsx3.init()
+
+    voices = engineio.getProperty('voices')
+    engineio.setProperty('rate', 130)
+    engineio.setProperty('voice', voices[1].id)  # 0 - polski, 1 - angielski
+
+    sentence = text
+
+    engineio.say(sentence)
+    engineio.runAndWait()
+
+def recognition():
+
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('Speak command: ')
+        audio = r.listen(source)
+
+        try:
+            text = r.recognize_google(audio)
+            # text = r.recognize_sphinx(audio)
+            #text = r.recognize_bing(audio)
+            #text = r.recognize_houndify(audio)
+            #text = r.recognize_ibm(audio)
+            # text = r.recognize_wit(audio)
+            return text
+        except:
+            print('Couldn\'t recognize')
+            recognition()
+
+#synthesis('south')
+# x = recognition()
+# print(x)
